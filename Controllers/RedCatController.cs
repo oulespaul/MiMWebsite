9ee1e14bc841a6cdbbc9ee5dd1647f59adfb6a4d
RedCat <590307030044@dpu.ac.th>
2019-11-01 14:08:39 +0700

    insert DB

--- Controllers/RedCatController.cs
+++ Controllers/RedCatController.cs
@@ -34,28 +34,15 @@ namespace MiMWebsite.Controllers
 			return View();
 		}
 		[HttpPost("create")]
-		public async Task<IActionResult> Create(UserModel model, IFormFile fileUpload)
+		public async Task<IActionResult> Create([FromBody]UserModel model)
 		{
-			string pathImgUser = "/image/user/";
-			string pathSave = $"wwwroot{pathImgUser}";
-			if (!Directory.Exists(pathSave))
-			{
-				Directory.CreateDirectory(pathSave);
-			}
-			if (fileUpload != null)
-			{
-				string extFile = Path.GetExtension(fileUpload.ContentDisposition);
-
-				var path = Path.Combine(Directory.GetCurrentDirectory());
-
-				using (var stream = new FileStream(path, FileMode.Create))
-				{
-					await fileUpload.CopyToAsync(stream);
-				}
-			}
+		
 				db.User.Add(model);
 				await db.SaveChangesAsync();
-				return RedirectToAction("Index");
+				return Ok(new
+				{
+					message = "Insert success"
+				});
 				
 			
 		}
